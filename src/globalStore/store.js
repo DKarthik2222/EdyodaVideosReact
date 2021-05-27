62583a9a465962c1c09f9938628ecfa87cc8a622
DKarthik2222 <karthikdulam2222@gmail.com>
2021-05-27 17:20:34 +0530

    api updates watch page

--- src/globalStore/store.js
+++ src/globalStore/store.js
@@ -1,59 +1,71 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-// import axios from "axios";
+import axios from "axios";
+import { baseUrl } from "../CommonResource/Common";
 
-// export const getProducts = () => {
-//   return (dispatch) => {
-//     axios
-//       .get(`https://5fc0748dfd14be0016749cfe.mockapi.io/karthik/acme`)
-//       .then((res) => {
-//         // console.log(res.data);
-//         dispatch({
-//           type: "GET_PRODUCTS",
-//           payload: res.data,
-//         });
-//       });
-//   };
-// };
-export const userLoggedIn = (payload) => {
+export const callAllVideos = () => {
+    return (dispatch) => {
+      axios
+        .get(`${baseUrl}`)
+        .then((res) => {
+          dispatch({
+            type: "ALL_VIDEOS",
+            payload: res.data,
+          });
+        });
+    };
+  };
+export const userLoggedIn = payload => {
   return {
     type: "USER_LOGIN",
-    payload,
+    payload
   };
 };
-export const educatorLoggedIn = (payload) => {
+export const educatorLoggedIn = payload => {
   return {
     type: "EDUCATOR_LOGIN",
-    payload,
+    payload
   };
 };
-export const storeUserData = (payload) => {
+export const storeUserData = payload => {
   return {
     type: "USER_DATA",
-    payload,
+    payload
   };
 };
 
 const initialState = {
   userData: JSON.parse(window.localStorage.getItem("userData")) || {},
   userLogin: window.localStorage.getItem("userLogged") || false,
-  educatorLogin: window.localStorage.getItem("educatorLogged") || false
+  educatorLogin: window.localStorage.getItem("educatorLogged") || false,
+  allVideoStore: []
 };
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    
     case "USER_LOGIN":
-        window.localStorage.setItem("userLogged", payload);
-        return { ...state, userLogin: window.localStorage.getItem("userLogged") };
+      window.localStorage.setItem("userLogged", payload);
+      return { ...state, userLogin: window.localStorage.getItem("userLogged") };
 
     case "EDUCATOR_LOGIN":
-        window.localStorage.setItem("educatorLogged", payload);
-        return { ...state, educatorLogin: window.localStorage.getItem("educatorLogged") };
-    
-        case "USER_DATA":
-        window.localStorage.setItem("userData", JSON.stringify(payload));
-        return { ...state, userData: JSON.parse(window.localStorage.getItem("userData")) };
+      window.localStorage.setItem("educatorLogged", payload);
+      return {
+        ...state,
+        educatorLogin: window.localStorage.getItem("educatorLogged")
+      };
+
+    case "USER_DATA":
+      window.localStorage.setItem("userData", JSON.stringify(payload));
+      return {
+        ...state,
+        userData: JSON.parse(window.localStorage.getItem("userData"))
+      };
+
+    case "ALL_VIDEOS":
+      return {
+        ...state,
+        allVideoStore: payload
+      };
 
     default:
       return state;
